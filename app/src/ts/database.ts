793cc0767b3177e8faccb2e9166e23056427ee8b
Fernando Furtado <fppfurtado@outlook.com>
2024-09-18 13:20:04 -0300

    ajusta projeto para iniciar corretamente banco de dados sqlite

--- app/src/ts/database.ts
+++ app/src/ts/database.ts
@@ -1,5 +1,8 @@
 import { Database, open } from 'sqlite';
 import sqlite3 from 'sqlite3';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { readFileSync } from 'fs';
 
 type book = {
   readonly id?: number,
@@ -9,14 +12,7 @@ type book = {
   year?: string
 }
 
-let database: Database;
-
-open({
-    filename: './db.sqlite',
-    driver: sqlite3.Database
-  }).then((db) => {
-    database = db;
-  })
+const database: Database = await startDatabase();
 
 export async function listBooks(): Promise<book[]> {
   return await database.all('SELECT * FROM book');
@@ -47,4 +43,19 @@ export async function updateBook(book: book): Promise<any> {
 export async function deleteBook(id: string): Promise<any> {
   const sql = 'DELETE FROM book WHERE id = ?';
   return database.run(sql, [id]);
+}
+
+async function startDatabase(): Promise<Database> {
+  
+  const filePath = fileURLToPath(import.meta.url);
+  const sqlFilePath = dirname(dirname(filePath));
+  const createTableSql = readFileSync(`${sqlFilePath}/sql/ddl_create_book_table.sql`).toString();
+  
+  return await open({
+      filename: './app/dist/db.sqlite',
+      driver: sqlite3.Database
+    }).then((db) => {
+      db.exec(createTableSql);
+      return db;
+    });
 }
