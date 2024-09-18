80ddf7bfe12073049a053ab3cb621a6d4c55ac9a
Fernando Furtado <fppfurtado@outlook.com>
2024-09-18 12:17:56 -0300

    criada operação DELETE  e ajustadas as assinaturas das funções para declarar e tratar Promises

--- app/src/database.ts
+++ app/src/database.ts
@@ -18,15 +18,15 @@ open({
     database = db;
   })
 
-export async function listBooks() {
+export async function listBooks(): Promise<book[]> {
   return await database.all('SELECT * FROM book');
 }
 
-export async function findBookById(id: string) {
+export async function findBookById(id: string): Promise<book[] | undefined> {
   return await database.get('SELECT * FROM book WHERE id = ?', id);
 }
 
-export async function createBook(book:book) {
+export async function createBook(book:book): Promise<any> {
   return await database.run(
     'INSERT INTO book(title, author, isbn, year) VALUES (:title, :author, :isbn, :year)',
     {
@@ -38,8 +38,13 @@ export async function createBook(book:book) {
   )
 }
 
-export async function updateBook(book: book) {
+export async function updateBook(book: book): Promise<any> {
   const { id, title, author, isbn, year } = book;
   const sql = 'UPDATE book SET title = ?, author = ?, isbn = ?, year = ? WHERE id = ?';
   return await database.run(sql, [title, author, isbn, year, id]);
+}
+
+export async function deleteBook(id: string): Promise<any> {
+  const sql = 'DELETE FROM book WHERE id = ?';
+  return database.run(sql, [id]);
 }
--- app/src/server.ts
+++ app/src/server.ts
@@ -9,25 +9,36 @@ server.get('/', (req, res) => {
 });
 
 server.get('/livros', async (req, res) => {
-    const livros = await dao.listBooks();
-    res.status(200).json(livros);
+    const books = await dao.listBooks();
+    res.status(200).json(books);
 });
 
 server.get('/livros/:id', async (req, res) => {
-    const livros = await dao.findBookById(req.params.id);
-    res.status(200).json(livros);
+    const books = await dao.findBookById(req.params.id);
+    res.status(200).json(books);
 });
 
 server.post('/livros', (req, res) => {
-    const novoLivro = req.body;
-    dao.createBook(novoLivro);
-    res.status(201).send(`livro ${novoLivro.title} criado com sucesso`);
+    const bookFromRequest = req.body;
+    dao.createBook(bookFromRequest)
+    .then((stmt) => res.status(201).send(`livro ${bookFromRequest.title} criado com sucesso`))
+    .catch((error) => res.status(500).send(`erro ao criar o livro ${bookFromRequest.title}`));
+    
 });
 
 server.put('/livros/:id', (req, res) => {
-    const livroAtualizado = req.body;
-    dao.updateBook(livroAtualizado);
-    res.status(200).send(`livro id ${livroAtualizado.id} atualizado com sucesso`);
-})
+    const bookFromRequest = req.body;
+    dao.updateBook(bookFromRequest)
+    .then((stmt) => res.status(200).send(`book id ${bookFromRequest.id} atualizado com sucesso`))
+    .catch((error) => res.status(500).send(`erro ao atualizar o livro ${bookFromRequest.title}`));
+    
+});
+
+server.delete('/livros/:id', (req, res) => {
+    const { id } = req.params; 
+    dao.deleteBook(id)
+    .then((stmt) => res.status(200).send(`o livro id ${id} foi deletado com sucesso`))
+    .catch((error) => res.status(500).send(`error ao deletar o livro id ${id}`));
+});
 
 export default server;
